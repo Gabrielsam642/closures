d50158a574be919c26612dfe2bb31eff734e45c4
Gabriel Samuel <samuelgab246@gmail.com>
2024-02-07 08:09:45 -0800

    Advanced function

---
+++ script.js
@@ -0,0 +1,11 @@
+const first = () => {
+        const greet = 'hello there how are doing';
+    const Second = () => {
+        const name = "Samuel";
+        alert(greet);
+    }
+    return Second;
+}
+
+const newFunc = first();
+newFunc();
